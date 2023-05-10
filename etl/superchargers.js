723b1030baf335305e12d1acb7919264155819e9
Alexander Edwards <alexanderedwards@Alexanders-Air.lan>
2023-05-09 19:08:16 -0500

    async funciton wrap

--- etl/superchargers.js
+++ etl/superchargers.js
@@ -1,42 +1,52 @@
 const axios = require('axios');
 const MongoClient = require('mongodb').MongoClient;
-const response = await axios.get('https://supercharge.info/service/supercharge/allSites');
-const data = response.data;
-const client = new MongoClient('mongodb://0.0.0.0:27017/');
-await client.connect();
-const db = client.db('charge-chat');
-const collection = db.collection('superchargers');
 
-const radius = 40 / 111000; // Convert yards to roughly degrees
-const sides = 36; // Number of polygon sides
+async function loadSuperchargerData() {
+  const response = await axios.get('https://supercharge.info/service/supercharge/allSites');
+  const data = response.data;
+  const client = new MongoClient('mongodb://0.0.0.0:27017/');
+  await client.connect();
+  const db = client.db('charge-chat');
+  const collection = db.collection('superchargers');
 
-for (const item of data) {
+  const radius = 40 / 111000; // Convert yards to roughly degrees
+  const sides = 36; // Number of polygon sides
+
+  for (const item of data) {
     console.log('loading item: ', item);
-  // Create a circular polygon
-  const polygon = {
-    type: 'Polygon',
-    coordinates: [[]]
-  };
-  
-  for (let i = 0; i <= sides; i++) {
-    const angle = 2 * Math.PI * i / sides;
-    const dx = radius * Math.cos(angle);
-    const dy = radius * Math.sin(angle);
-    polygon.coordinates[0].push([item.gps.longitude + dx, item.gps.latitude + dy]);
+    // Create a circular polygon
+    const polygon = {
+      type: 'Polygon',
+      coordinates: [[]]
+    };
+
+    for (let i = 0; i <= sides; i++) {
+      const angle = 2 * Math.PI * i / sides;
+      const dx = radius * Math.cos(angle);
+      const dy = radius * Math.sin(angle);
+      polygon.coordinates[0].push([item.gps.longitude + dx, item.gps.latitude + dy]);
+    }
+
+    // Insert the document into MongoDB
+    await collection.updateOne(
+      { id: item.id },
+      {
+        $setOnInsert: { id: item.id },
+        $set: {
+          location: polygon,
+          // Include any other fields you want to store
+        }
+      },
+      { upsert: true }
+    );
   }
 
-  // Insert the document into MongoDB
-  await collection.updateOne(
-    { id: item.id },
-    {
-      $setOnInsert: { id: item.id },
-      $set: {
-        location: polygon,
-        // Include any other fields you want to store
-      }
-    },
-    { upsert: true }
-  );
+  await client.close();
 }
 
-await client.close();
+// Call the function
+loadSuperchargerData().then(() => {
+  console.log('Supercharger data loaded successfully.');
+}).catch(error => {
+  console.error('Error loading supercharger data:', error);
+});
