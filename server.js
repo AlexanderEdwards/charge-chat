9e79fc78c12dd97ec8c3a0c46e233766b32f8311
Alexander Edwards <alexanderedwards@Alexanders-Air.lan>
2023-05-12 22:40:51 -0500

    server socket changes

--- server.js
+++ server.js
@@ -1,8 +1,38 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 const port = 3000;
+
+const wss = new WebSocket.Server({ noServer: true });
+
+const sessions = {};
+
+wss.on('connection', (ws, req) => {
+  const superchargerId = req.url.slice(1);
+
+  if (!sessions[superchargerId]) {
+    sessions[superchargerId] = { clients: new Set(), count: 0 };
+  }
+
+  const sessionId = sessions[superchargerId].count++;
+  sessions[superchargerId].clients.add(ws);
+
+  ws.on('message', (message) => {
+    sessions[superchargerId].clients.forEach((client) => {
+      if (client !== ws) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    sessions[superchargerId].clients.delete(ws);
+
+    if (sessions[superchargerId].clients.size === 0) {
+      delete sessions[superchargerId];
+    }
+  });
+});
 
 async function findSupercharger(latitude, longitude) {
   const client = new MongoClient('mongodb://0.0.0.0:27017/');
@@ -45,11 +75,24 @@ app.get('/findSupercharger', async (req, res) => {
     res.status(200).json({id: supercharger.id, name: supercharger.name});
   } else {
     res.status(204).json({});
-  }
+  } 
 });
 
 app.use(express.static('public'));
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
+});
+
+server.on('upgrade', (request, socket, head) => {
+  const pathname = request.url;
+
+  if (!pathname.startsWith('/')) {
+    socket.destroy();
+    return;
+  }
+
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit('connection', ws, request);
+  });
 });
